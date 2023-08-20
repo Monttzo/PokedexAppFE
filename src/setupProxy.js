72757d9418821f09ed3fedb0bf9094ac031ca408
Daniel Pulido <dpulido59@gmail.com>
2023-08-20 10:01:53 -0500

    Change setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = (app) => {
-    app.use(createProxyMiddleware('/api2', {
-        target: 'http://54.236.255.171' ,
-        changeOrigin: true,
-        pathRewrite: { '^/api2': '' }
+
+module.exports = function(app) {
+  app.use(
+    '/api2', // Esto debe coincidir con el prefijo en las solicitudes
+    createProxyMiddleware({
+      target: 'http://54.236.255.171',
+      changeOrigin: true,
+      pathRewrite: { '^/api2': '' } // Esto elimina el prefijo /api2 en las solicitudes reales
     })
-    );
-}
+  );
+};
